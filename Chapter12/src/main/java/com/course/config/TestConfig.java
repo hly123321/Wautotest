667fb6eeafaebf784d6b6ab5d6965834e1aea6f9
liangyu.hu <36560764@qq.com>
2018-09-19 16:47:02 +0800

    java接口自动化测试

---
+++ Chapter12/src/main/java/com/course/config/TestConfig.java
@@ -0,0 +1,16 @@
+package com.course.config;
+
+import org.apache.http.client.CookieStore;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+//该工具类帮忙直接拼接url
+public class TestConfig {
+    public static String loginUrl;
+    public static String updateUserInfoUrl;
+    public static String getUserInfoUrl;
+    public static String getUserListUrl;
+    public static String addUserUrl;
+
+    public static DefaultHttpClient defaultHttpClient;
+    public static CookieStore store;
+}
